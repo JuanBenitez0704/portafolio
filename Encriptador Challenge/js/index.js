a206e1c0373eb3ebb7fd0f5cb1d3ae354e5fec45
JuanBenitez0704 <jmbenitez0704@gmail.com>
2023-05-30 21:49:13 -0500

    modificacion js

--- Encriptador Challenge/js/index.js
+++ Encriptador Challenge/js/index.js
@@ -1,19 +1,46 @@
-const textArea = document.querySelector(".text-area");
-const textEncripted = document.querySelector(".text-encripted");
+const textArea = document.querySelector(".text-box");
+const mensaje = document.querySelector(".text-mensaje");
+const copia = document.querySelector(".btn-copiar");
+copia.style.display = "none";
+
+
+function validarTexto(){
+    let textoEscrito = document.querySelector(".text-area").value;
+    let validador = textoEscrito.match(/^[a-z]*$/);
+
+    if(!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos")
+        location.reload();
+        return true;
+    }
+}
+
 
 function btnEncriptar(){
-    const textoEncriptado = encriptar(textArea.value)
-    textEncripted.value = textoEncriptado;
-    textArea.value = "";
-    textEncripted.style.backgroundImage = "none";
+    if(!validarTexto()) {
+        const textoEncriptado = encriptar(textArea.value);
+        mensaje.value = textoEncriptado;
+        mensaje.style.backgroundImage = "none";
+        textArea.value = "";
+        copia.style.display = "block";
+    
+    }
 }
 
 function btnDesencriptar(){
-    const textoDesencriptado = desencriptar(textArea.value);
+    const textoDesencriptado = desencriptar(textArea.value)
     textEncripted.value = textoDesencriptado; 
     textArea.value = "";
-    textEncripted.style.backgroundImage = "none";
+    textArea.value = "";
 }
+
+function btnCopiar(){
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value = "";
+    alert("Texto Copiado")
+}
+
 
 function encriptar(stringTextArea){
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
@@ -37,4 +64,5 @@ function desencriptar(stringEncripted){
     }
     return stringEncripted;
 }
+
 
