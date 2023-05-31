411f54476bc193b49e354d200fb8135b7a01ca75
JuanBenitez0704 <jmbenitez0704@gmail.com>
2023-05-30 21:24:19 -0500

    optimizing appearance and class names

--- Encriptador Challenge/js/index.js
+++ Encriptador Challenge/js/index.js
@@ -1,1 +1,40 @@
-const d = document
+const textArea = document.querySelector(".text-area");
+const textEncripted = document.querySelector(".text-encripted");
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    textEncripted.value = textoEncriptado;
+    textArea.value = "";
+    textEncripted.style.backgroundImage = "none";
+}
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar(textArea.value);
+    textEncripted.value = textoDesencriptado; 
+    textArea.value = "";
+    textEncripted.style.backgroundImage = "none";
+}
+
+function encriptar(stringTextArea){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringTextArea = stringTextArea.toLowerCase();
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if (stringTextArea.includes(matrizCodigo[i][0])) {
+            stringTextArea = stringTextArea.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringTextArea;
+}
+
+function desencriptar(stringEncripted){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncripted = stringEncripted.toLowerCase();
+   
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if (stringEncripted.includes(matrizCodigo[i][1])) {
+            stringEncripted = stringEncripted.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringEncripted;
+}
+
